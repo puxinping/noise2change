3d62e3caccb701263b90c9b53a0f97c8c6978859
Pu Xinping <1042754389@users.noreply.github.com>
2026-05-05 11:45:52 +0800

    chore(train): set batch size and switch to single GPU

--- scripts/train/mask/train_mask_generator.sh
+++ scripts/train/mask/train_mask_generator.sh
@@ -1,12 +1,27 @@
 export PYTHONPATH="${PYTHONPATH:-}:$(pwd)"
-CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nproc_per_node=2 scripts/train/mask/train_mask_generator.py \
+CUDA_VISIBLE_DEVICES=0 TORCH_DISTRIBUTED_DEBUG=DETAIL \
+python scripts/train/mask/train_mask_generator.py \
     --outdir=training-runs/00000-edm2-mask512-pixels-xs \
     --data=./datasets \
     --preset=edm2-img512-xs \
-    --batch-gpu=128 \
-    --batch=256 \
-    --status='10Ki'\
-    --snapshot='1Mi'\
-    --checkpoint='1Mi'\
+    --batch-gpu=4 \
+    --batch=4 \
+    --status='10Ki' \
+    --snapshot='1Mi' \
+    --checkpoint='1Mi' \
     --lr=0.001 \
-    --duration='50Mi' 
+    --duration='50Mi'
+
+
+# export PYTHONPATH="${PYTHONPATH:-}:$(pwd)"
+# CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nproc_per_node=2 scripts/train/mask/train_mask_generator.py \
+#     --outdir=training-runs/00000-edm2-mask512-pixels-xs \
+#     --data=./datasets \
+#     --preset=edm2-img512-xs \
+#     --batch-gpu=128 \
+#     --batch=256 \
+#     --status='10Ki'\
+#     --snapshot='1Mi'\
+#     --checkpoint='1Mi'\
+#     --lr=0.001 \
+#     --duration='50Mi' 

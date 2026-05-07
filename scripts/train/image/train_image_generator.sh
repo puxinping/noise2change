92e1f6f7cf578af7d0847cc016bbb7160620bc7c
Pu Xinping <puxinping@users.noreply.github.com>
2026-05-07 15:05:43 +0800

    chore(weights): update edm2-mask512-pixels-xs checkpoint

--- scripts/train/image/train_image_generator.sh
+++ scripts/train/image/train_image_generator.sh
@@ -1,15 +1,15 @@
 export PYTHONPATH="${PYTHONPATH:-}:$(pwd)"
-CUDA_VISIBLE_DEVICES=0,1 torchrun --standalone --nproc_per_node=2 scripts/train/image/train_image_generator.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --standalone --nproc_per_node=4 scripts/train/image/train_image_generator.py \
     --outdir=training-runs/00000-edm2-img512-control-xs \
     --data=./datasets \
     --preset=edm2-img512-xs \
-    --batch-gpu=64 \
-    --batch=128 \
+    --batch-gpu=8 \
+    --batch=32 \
     --status='10Ki'\
-    --snapshot='1Mi'\
-    --checkpoint='1Mi'\
+    --snapshot='10Ki'\
+    --checkpoint='10Ki'\
     --lr=0.001 \
-    --duration='200Mi' \
+    --duration='100Ki' \
     --dropout=0.1
 
 
